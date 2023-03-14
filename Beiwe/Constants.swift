f33080bcee7dc9bd935736123ba1c6061a7c5a66
Eli Jones <biblicabeebli@gmail.com>
2023-03-14 05:01:48 -0400

    [eli-fixes] cleanup and documentation.

--- Beiwe/Constants.swift
+++ Beiwe/Constants.swift
@@ -1,14 +1,5 @@
-//
-//  Constants.swift
-//  Beiwe
-//
-//  Created by Keary Griffin on 3/23/16.
-//  Copyright © 2016 Rocketfarm Studios. All rights reserved.
-//
-
 struct Constants {
-    static let passwordRequirementRegex = "^.{6,}$";
-    static let passwordRequirementDescription = NSLocalizedString("password_length_requirement", comment: "");
-
-    static let defaultStudyId = "default";
+    static let passwordRequirementRegex = "^.{6,}$"
+    static let passwordRequirementDescription = NSLocalizedString("password_length_requirement", comment: "")
+    static let defaultStudyId = "default"  // FIXME: purge
 }
--- Beiwe/Managers/PersistentPasswordManager.swift
+++ Beiwe/Managers/PersistentPasswordManager.swift
@@ -1,51 +1,54 @@
-//
-//  PersistentPasswordManager
-//  Beiwe
-//
-//  Created by Keary Griffin on 3/21/16.
-//  Copyright © 2016 Rocketfarm Studios. All rights reserved.
-//
-
 import Foundation
-import KeychainSwift;
+import KeychainSwift
+
+/// standardized name format
+fileprivate func keyForStudy(_ study: String, prefix: String) -> String {
+    return prefix + study
+}
 
 struct PersistentPasswordManager {
-    static let sharedInstance = PersistentPasswordManager();
-    static let bundlePrefix = (Bundle.main.bundleIdentifier ?? "com.rocketarmstudios.beiwe")
+    static let sharedInstance = PersistentPasswordManager()  // static singleton reference
+    static let bundlePrefix = Bundle.main.bundleIdentifier ?? "com.rocketarmstudios.beiwe"
+
+    // Apple Keychain connection for secure storage
     fileprivate let keychain: KeychainSwift
-    fileprivate let passwordKeyPrefix = "password:";
-    fileprivate let rsaKeyPrefix = PersistentPasswordManager.bundlePrefix + ".rsapk.";
+
+    // file name prefixes
+    fileprivate let passwordKeyPrefix = "password:"
+    fileprivate let rsaKeyPrefix = PersistentPasswordManager.bundlePrefix + ".rsapk."
 
     init() {
+        // instantiate the keychain depending on development environment (TODO: why do we do this?)
         #if targetEnvironment(simulator)
-             keychain = KeychainSwift(keyPrefix: PersistentPasswordManager.bundlePrefix + ".")
+            self.keychain = KeychainSwift(keyPrefix: PersistentPasswordManager.bundlePrefix + ".")
         #else
-            keychain = KeychainSwift()
+            self.keychain = KeychainSwift()
         #endif
     }
 
-    fileprivate func keyForStudy(_ study: String, prefix: String) -> String {
-        let key = prefix + study;
-        return key;
-    }
-
+    /// Gets the password for the current study, but is optional
     func passwordForStudy(_ study: String = Constants.defaultStudyId) -> String? {
         // passwords are stored on a per-study basis, so in principle someone could have multiple instances of study information present.
-        return keychain.get(keyForStudy(study, prefix: passwordKeyPrefix));
+        return self.keychain.get(keyForStudy(study, prefix: self.passwordKeyPrefix))
     }
 
+    /// Sets the password string for the participant on a particular study in the keychain for secure storage, device-only (not icloud shared?)
+    /// - study - FIXME: study has a stupid default value, purge.
     func storePassword(_ password: String, study: String = Constants.defaultStudyId) {
-        keychain.set(password, forKey: keyForStudy(study, prefix: passwordKeyPrefix), withAccess: .accessibleAlwaysThisDeviceOnly);
+        self.keychain.set(
+            password, forKey: keyForStudy(study, prefix: self.passwordKeyPrefix), withAccess: .accessibleAlwaysThisDeviceOnly
+        )
     }
 
+    /// Gets the SecKey object (RSA encryption operations) for the partcipant
+    ///  - study - FIXME: this default value is a hardcoded constant and there is one call to this function
     func storePublicKeyForStudy(_ publicKey: String, patientId: String, study: String = Constants.defaultStudyId) throws -> SecKey {
-        let keyref = try SwiftyRSA.storePublicKey(publicKey, keyId: publicKeyName(patientId, study: study))
+        let keyref = try SwiftyRSA.storePublicKey(publicKey, keyId: self.publicKeyName(patientId, study: study))
         return keyref
     }
 
+    /// Gets the name of the participant's RSA key
     func publicKeyName(_ patientId: String, study: String = Constants.defaultStudyId) -> String {
-        return keyForStudy(study, prefix: rsaKeyPrefix) + "." + patientId;
+        return keyForStudy(study, prefix: self.rsaKeyPrefix) + "." + patientId
     }
-
-
 }
