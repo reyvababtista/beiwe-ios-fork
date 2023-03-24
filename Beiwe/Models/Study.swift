ddb151b3db325d277a39cecb2f1ec63beedf2a41
Eli Jones <biblicabeebli@gmail.com>
2023-03-23 20:35:02 -0400

    [eli-fixes] automated cleanup of study class

--- Beiwe/Models/Study.swift
+++ Beiwe/Models/Study.swift
@@ -1,101 +1,89 @@
-//
-//  Study.swift
-//  Beiwe
-//
-//  Created by Keary Griffin on 3/27/16.
-//  Copyright © 2016 Rocketfarm Studios. All rights reserved.
-//
-
 import Foundation
-import ObjectMapper;
+import ObjectMapper
 
-class Study : ReclineObject {
-
-    var studyId = Constants.defaultStudyId;
-    var patientPhoneNumber: String = "";
-    var studySettings: StudySettings?;
-    var patientId: String?;
-    var clinicianPhoneNumber: String?;
-    var raPhoneNumber: String?;
-    var nextUploadCheck: Int64?;
-    var nextSurveyCheck: Int64?;
+class Study: ReclineObject {
+    var studyId = Constants.defaultStudyId
+    var patientPhoneNumber: String = ""
+    var studySettings: StudySettings?
+    var patientId: String?
+    var clinicianPhoneNumber: String?
+    var raPhoneNumber: String?
+    var nextUploadCheck: Int64?
+    var nextSurveyCheck: Int64?
     var lastUploadSuccess: Int64 = 0
     var missedSurveyCheck: Bool = false
     var missedUploadCheck: Bool = false
-    var lastBadgeCnt = 0;
-    var registerDate: Int64?;
-    var receivedAudioSurveys: Int = 0;
-    var receivedTrackingSurveys: Int = 0;
-    var submittedAudioSurveys: Int = 0;
-    var submittedTrackingSurveys: Int = 0;
-    var customApiUrl: String?;
-    var fuzzGpsLongitudeOffset: Double = 0.0;
-    var fuzzGpsLatitudeOffset: Double = 0.0;
+    var lastBadgeCnt = 0
+    var registerDate: Int64?
+    var receivedAudioSurveys: Int = 0
+    var receivedTrackingSurveys: Int = 0
+    var submittedAudioSurveys: Int = 0
+    var submittedTrackingSurveys: Int = 0
+    var customApiUrl: String?
+    var fuzzGpsLongitudeOffset: Double = 0.0
+    var fuzzGpsLatitudeOffset: Double = 0.0
+    
+    var surveys: [Survey] = []
+    var activeSurveys: [String: ActiveSurvey] = [:]
 
-    var surveys: [Survey] = [ ];
-    var activeSurveys: [String:ActiveSurvey] = [:]
-
-
-    var participantConsented: Bool = false;
+    var participantConsented: Bool = false
 
     init(patientPhone: String, patientId: String, studySettings: StudySettings, apiUrl: String?, studyId: String = Constants.defaultStudyId) {
-        super.init();
-        self.patientPhoneNumber = patientPhone;
-        self.studySettings = studySettings;
-        self.studyId = studyId;
-        self.patientId = patientId;
-        self.registerDate = Int64(Date().timeIntervalSince1970);
-        self.customApiUrl = apiUrl;
+        super.init()
+        self.patientPhoneNumber = patientPhone
+        self.studySettings = studySettings
+        self.studyId = studyId
+        self.patientId = patientId
+        self.registerDate = Int64(Date().timeIntervalSince1970)
+        self.customApiUrl = apiUrl
     }
 
     required init?(map: Map) {
-        super.init(map: map);
-
+        super.init(map: map)
     }
-    
+
     // Mappable
     override func mapping(map: Map) {
-        super.mapping(map: map);
-        patientPhoneNumber     <- map["phoneNumber"];
-        studySettings   <- map["studySettings"];
-        studyId   <- map["studyId"];
-        patientId <- map["patientId"];
-        participantConsented <- map["participantConsented"];
-        clinicianPhoneNumber <- map["clinicianPhoneNumber"];
-        raPhoneNumber <- map["raPhoneNumber"];
-        nextSurveyCheck <- (map["nextSurveyCheck"], TransformOf<Int64, NSNumber>(fromJSON: { $0?.int64Value }, toJSON: { $0.map { NSNumber(value: $0) } }))
-        nextUploadCheck <- (map["nextUploadCheck"], TransformOf<Int64, NSNumber>(fromJSON: { $0?.int64Value }, toJSON: { $0.map { NSNumber(value: $0) } }))
-        surveys    <- map["surveys"];
-        activeSurveys   <- map["active_surveys"]
-        registerDate <- (map["registerDate"], TransformOf<Int64, NSNumber>(fromJSON: { $0?.int64Value }, toJSON: { $0.map { NSNumber(value: $0) } }))
-        receivedAudioSurveys <- map["receivedAudioSurveys"]
-        receivedTrackingSurveys <- map["receivedTrackingSurveys"]
-        submittedAudioSurveys  <- map["submittedAudioSurveys"]
-        submittedTrackingSurveys <- map["submittedTrackingSurveys"]
-        missedSurveyCheck <- map["missedSurveyCheck"]
-        missedUploadCheck <- map["missedUploadCheck"]
-        lastUploadSuccess <- map["lastUploadSuccess"]
-        customApiUrl <- map["customApiUrl"]
-        fuzzGpsLongitudeOffset <- map["fuzzGpsLongitudeOffset"]
-        fuzzGpsLatitudeOffset <- map["fuzzGpsLatitudeOffset"]
+        super.mapping(map: map)
+        self.patientPhoneNumber <- map["phoneNumber"]
+        self.studySettings <- map["studySettings"]
+        self.studyId <- map["studyId"]
+        self.patientId <- map["patientId"]
+        self.participantConsented <- map["participantConsented"]
+        self.clinicianPhoneNumber <- map["clinicianPhoneNumber"]
+        self.raPhoneNumber <- map["raPhoneNumber"]
+        self.nextSurveyCheck <- (map["nextSurveyCheck"], TransformOf<Int64, NSNumber>(fromJSON: { $0?.int64Value }, toJSON: { $0.map { NSNumber(value: $0) } }))
+        self.nextUploadCheck <- (map["nextUploadCheck"], TransformOf<Int64, NSNumber>(fromJSON: { $0?.int64Value }, toJSON: { $0.map { NSNumber(value: $0) } }))
+        self.surveys <- map["surveys"]
+        self.activeSurveys <- map["active_surveys"]
+        self.registerDate <- (map["registerDate"], TransformOf<Int64, NSNumber>(fromJSON: { $0?.int64Value }, toJSON: { $0.map { NSNumber(value: $0) } }))
+        self.receivedAudioSurveys <- map["receivedAudioSurveys"]
+        self.receivedTrackingSurveys <- map["receivedTrackingSurveys"]
+        self.submittedAudioSurveys <- map["submittedAudioSurveys"]
+        self.submittedTrackingSurveys <- map["submittedTrackingSurveys"]
+        self.missedSurveyCheck <- map["missedSurveyCheck"]
+        self.missedUploadCheck <- map["missedUploadCheck"]
+        self.lastUploadSuccess <- map["lastUploadSuccess"]
+        self.customApiUrl <- map["customApiUrl"]
+        self.fuzzGpsLongitudeOffset <- map["fuzzGpsLongitudeOffset"]
+        self.fuzzGpsLatitudeOffset <- map["fuzzGpsLatitudeOffset"]
     }
-    
+
     func surveyExists(surveyId: String?) -> Bool {
-        for survey in surveys {
+        for survey in self.surveys {
             if survey.surveyId == surveyId {
                 return true
             }
         }
         return false
     }
-    
+
     func getSurvey(surveyId: String?) -> Survey? {
-        for survey in surveys {
+        for survey in self.surveys {
             if survey.surveyId == surveyId {
                 return survey
             }
         }
         return nil
     }
-
 }
